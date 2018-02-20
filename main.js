7555560b26ac85a2041b562401d7845c0d7fce27
Ryan <jragard@gmail.com>
2018-02-20 16:57:08 -0500

    added 'next steps' section

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 var currentPlayer = "X";
 var nextPlayer = "O";
 
+var xScore = 0;
+var oScore = 0;
+var drawScore = 0;
+
+// Who's Turn it is
+let element = document.createElement("h1");
+let destination = document.getElementById("X");
+destination.appendChild(element);
+element.innerHTML = "X's Turn!"
+
+// X Score
+let elementX = document.createElement("h4");
+let destinationX = document.getElementById("xScore");
+destinationX.appendChild(elementX);
+elementX.innerHTML = "X: " + xScore;
+
+//O Score
+let elementO = document.createElement("h4");
+let destinationO = document.getElementById("oScore");
+destinationO.appendChild(elementO);
+elementO.innerHTML = "O: " + oScore;
+
+//Draw Score
+let elementDraw = document.createElement("h4");
+let destinationDraw = document.getElementById("draw");
+destinationDraw.appendChild(elementDraw);
+elementDraw.innerHTML = "Draw: " + drawScore;
+
+
 var playerXSelections = new Array();
 var playerOSelections = new Array();
 
@@ -29,9 +58,9 @@ function checkWinner(playerSelections) {
 
         if (matches === 3) {
             return true;
-            // console.log("Winner");
 
-        } 
+
+        }
     }
 }
 
@@ -42,8 +71,10 @@ function checkDraw() {
 function resetGame() {
     playerXSelections = new Array();
     playerOSelections = new Array();
-    for (var i = 0; i < cells.length; i++) {
-        cells[i].innerHTML = " "
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = " ";
+        cells[i].addEventListener('click', handleClick);
+
     }
 }
 
@@ -51,37 +82,62 @@ function resetGame() {
 
 handleClick = function (event) {
     var cell = event.target;
+    cell.className = "cellText";
+    // $(".cellText").fadeIn();
 
     cell.innerHTML = currentPlayer;
+    // cell.className = "cellText";
+    
+
+
 
     if (currentPlayer === "X") {
+        console.log("X");
+        element.innerHTML = "O's Turn!";
         playerSelections = playerXSelections;
         nextPlayer = "O";
     } else {
+        console.log("O");
+        element.innerHTML = "X's Turn!"
         playerSelections = playerOSelections;
         nextPlayer = "X";
     }
 
     playerSelections.push(parseInt(cell.id));
 
+    cell.removeEventListener('click', handleClick);
 
     if (checkWinner(playerSelections)) {
-        console.log(checkWinner(playerSelections));
-        alert("Player" + currentPlayer + "wins!");
+        alert("Player" + currentPlayer + "wins!"); 
+        if (currentPlayer === "X") 
+             xScore++; 
+             elementX.innerHTML = "X: " + xScore;
+        if (currentPlayer === "O") 
+             oScore++ 
+             elementO.innerHTML = "O: " + oScore;
         resetGame();
-    }
+        }   
+         
+        
+    
 
     if (checkDraw()) {
         alert("Draw!");
+        drawScore++
+        elementDraw.innerHTML = "Draw: " + drawScore;
         resetGame();
     }
+
+
+
     //Swap Players
 
     currentPlayer = nextPlayer;
+
 }
 
 var cells = document.querySelectorAll("td");
 
 for (var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', handleClick)
+    cells[i].addEventListener('click', handleClick);
 }
